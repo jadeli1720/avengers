e7983f89ef11e412d200a4b05d3bf58942f24b3e
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-24 00:02:18 -0600

    Using object history object to navigate to other routes programmatically

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 export default function AvengersList(props) {
@@ -12,14 +12,20 @@ export default function AvengersList(props) {
                 
                 <div className="character-card" key={avenger.id}>
                     <img src={avenger.thumbnail} alt={avenger.name} />
-                    <h2> 
-                        <Link to={`/avengers/${avenger.id}`}>{avenger.name}</Link>
-                    </h2>
+                    {/* <h2> 
+                        <Link to={`/avengers/${avenger.id}`}>{avenger.name}</Link> will be replaced with below code because function on line 27 does essentially the same thing
+                    </h2> */}
+                    <div className="character-name" onClick={() => routeToAvenger(props, avenger)}>{avenger.name}</div>
                     <p>({avenger.nickname})</p>
                 </div>
             ))}
             {console.log("AvengerList",props) /** This let me know that the array data was being passed to the avengerlist*/}
         </div>
     )
+}
+
+//Using plain vanilla js to use the built in history object
+function routeToAvenger(props, avenger) {
+    props.history.push(`/avengers/${avenger.id}`)
 }
 
