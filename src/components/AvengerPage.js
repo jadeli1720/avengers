825ab161cb3a0f8109e9713981ee34ab8f3a50ff
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-23 23:41:41 -0600

    Working on nested routes and created 2 new components: AvengerDetails.js and AvengerMovies.js

--- src/components/AvengerPage.js
+++ src/components/AvengerPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
-import { avengers } from '../Data/Data';
+import AvengerDetails from './AvengerDetails';
 
 export default function AvengerPage(props) {
     const id = props.match.params.id;
-    const avenger = avengers.find(avenger => `${avenger.id}` === id);
+    // Changes avengers.find to props.avengers.find() with render props
+    const avenger = props.avengers.find(avenger => `${avenger.id}` === id);
 
     return(
         <div className="avengerPage">
@@ -17,8 +19,14 @@ export default function AvengerPage(props) {
             <div className="character-info-wrapper">
                 <h1>{avenger.name}</h1>
                 <h4>{avenger.nickname}</h4>
-                <p>{avenger.description}</p>
+                {/* <p>{avenger.description}</p> */}
+                {/**Refactoring the above to add in nested Routes to the page. Then adding the components that correspond to the nested routes */}
             </div>
+            <Link to={`/avengers/${props.match.params.id}/details`} >Info</Link>
+            <Link to={`/avengers/${props.match.params.id}/movies`} >Movies</Link>
+
+            <Route path="/avengers/:id/details" render={props => <AvengerDetails details={avenger.description} />} />
+            <Route path="/avengers/:id/movies" render={props => } />
         </div>
     )
 }
